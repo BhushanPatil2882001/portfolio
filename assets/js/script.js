d93eac4ee0877cf925a7abb7551e66e2ceffab8b
BhushanPatil2882001 <sharewithbhushan@gmail.com>
2025-04-10 21:32:07 +0530

    more modify: hero

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,3 @@
-// custom cursor
-let body = document.querySelector("body");
-let cursor = document.querySelector(".cursor");
-
-cursor.style.opacity = 0;
-body.addEventListener("mousemove", (dets)=>{
-    cursor.style.opacity = 1;
-    cursor.style.left = dets.x+"px";
-    cursor.style.top = dets.y+"px"
-    setTimeout(()=>{
-        cursor.style.opacity = 0;
-    },3000)
-})
-
 // loading container
 
 let loading = document.querySelector(".preloader");
